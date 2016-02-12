8ace4f5e521489224c933e8488a8e0d8923cf280
Dane Hixson <dhixson@cove-productions.com>
2016-02-11 20:20:30 -0700

    Added code to create restful routes

--- tests/1455228873_route.py
+++ tests/1455228873_route.py
@@ -3,6 +3,7 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)).strip('tests'))
 import unittest
 from app.bin.application.Routes import Routes
+from app.controllers.TestController import TestController
 
 
 class Testroute(unittest.TestCase):
@@ -27,25 +28,25 @@ class Testroute(unittest.TestCase):
         """Test that a single get route can be saved"""
         route = Routes()
         route.get('/home', lambda: 8**2)
-        self.assertEqual(route.mapping['GET']['/home'](), 64)
+        self.assertEqual(route.mapping['GET']['/home']['no_param'](), 64)
 
     def test_single_route_post_no_param(self):
         """Test that a single post route can be saved"""
         route = Routes()
         route.post('/home', lambda: 8**2)
-        self.assertEqual(route.mapping['POST']['/home'](), 64)
+        self.assertEqual(route.mapping['POST']['/home']['no_param'](), 64)
 
     def test_single_route_put_no_param(self):
         """Test that a single put route can be saved"""
         route = Routes()
         route.put('/home', lambda: 8**2)
-        self.assertEqual(route.mapping['PUT']['/home'](), 64)
+        self.assertEqual(route.mapping['PUT']['/home']['no_param'](), 64)
 
     def test_single_route_delete_no_param(self):
         """Test that a single delete route can be saved"""
         route = Routes()
         route.delete('/home', lambda: 8**2)
-        self.assertEqual(route.mapping['DELETE']['/home'](), 64)
+        self.assertEqual(route.mapping['DELETE']['/home']['no_param'](), 64)
 
     def test_single_route_get_param(self):
         """Test that a single get route with param can be saved"""
@@ -71,7 +72,15 @@ class Testroute(unittest.TestCase):
         route.delete('/home', lambda x: x**2, True)
         self.assertEqual(route.mapping['DELETE']['/home']['param'](8), 64)
 
+    def test_resource_route(self):
+        route = Routes()
+        route.restful('/home', TestController)
+        self.assertEqual(route.mapping['GET']['/home']['no_param'](), 64)
+        self.assertEqual(route.mapping['GET']['/home']['param'](8), 64)
+        self.assertEqual(route.mapping['POST']['/home']['no_param'](), 64)
+        self.assertEqual(route.mapping['PUT']['/home']['param'](8), 64)
+        self.assertEqual(route.mapping['DELETE']['/home']['param'](8), 64)
+
 
 if __name__ == '__main__':
-    print "route"
     unittest.main()
